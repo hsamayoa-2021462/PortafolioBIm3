061383ed2edda46fb98d19a25af5ecaa6a586c05
hsamayoa-2021462 <hsamayoa-2021462@kinal.edu.gt>
2026-07-26 15:07:01 -0600

    Actualizar información de proyectos del portafolio

--- portafolio-hugo-samayoa/src/shared/data/projectsData.js
+++ portafolio-hugo-samayoa/src/shared/data/projectsData.js
@@ -6,7 +6,7 @@
 export const PROJECTS_DATA = [
   {
     id: 'proyecto-1',
-    titulo: '[ SpeedCam GT ]',
+    titulo: ' SpeedCam GT ',
     descripcion: ' Es un proyecto basado en colocar infracciones a las personas que exceden de velocidad y otras cosas, cuenta con perfil administrador y cliente para poder pagar las infracciones y ver si todo esta al dia o no. ',
     tecnologias: [' Node.js ', ' React native ', ' MongoDB '],
     github: 'https://github.com/jregil-2021564/ProyectoFinalCamara.git',
@@ -18,7 +18,7 @@ export const PROJECTS_DATA = [
   },
   {
     id: 'proyecto-2',
-    titulo: '[ Gestion de Restaurantes ]',
+    titulo: ' Gestion de Restaurantes ',
     descripcion: ' Aplicacion movil y web con interfaz de usuario y administador, donde se puede ver desde una mesa disponuble hasta el restaurante si esta disponible o no, con reservacion, platos y mas.',
     tecnologias: [' React Native ', ' Postgres ', ' Mongo Db ', ' Docker Desktop ', ' Vercel'],
     github: 'https://github.com/hsamayoa-2021462/ProyectoGRestaurantes3.git',
@@ -30,7 +30,7 @@ export const PROJECTS_DATA = [
   },
   {
     id: 'proyecto-3',
-    titulo: '[ Sistema Bancario ]',
+    titulo: ' Sistema Bancario ',
     descripcion: ' La plataforma bancaria más segura y eficiente de Guatemala.Con perfil admin y cliente,gestiona tu patrimonio con la confianza de tecnología de nivel mundial, cuenta con transferencias, depositos,estados de cuenta, gestion de cuenta.',
     tecnologias: [' React Native ', ' Postrgres ', ' MongoDB ', ' Node.js '],
     github: 'https://github.com/aarocha-2024203/ProyectoBancario.git',
@@ -42,14 +42,38 @@ export const PROJECTS_DATA = [
   },
   {
     id: 'proyecto-4',
-    titulo: '[ PowerFit Gym ]',
+    titulo: ' PowerFit Gym ',
     descripcion: ' Pagina Web hecha y utilizable para poder hacer gymnasio desde tu casa o cualquier lugar a tu gusto, con variedad de rutinas a tu eleccion.',
     tecnologias: [' Visual Estudio Code ', ' JavaScript ', ' Netlify '],
-    github: 'https://github.com/aarocha-2024203/ProyectoBancario.git',
+    github: 'https://github.com/hsamayoa-2021462/ProyectoGymFit.git',
     demo: 'https://hsamayoa2021462.netlify.app/',
     imagenes: [
       '/assets/images/projects/proyecto-4/captura-1.jpg',
       '/assets/images/projects/proyecto-4/captura-2.jpg',
+    ],
+  },
+  {
+    id: 'proyecto-5',
+    titulo: ' Mi Blog Personal ',
+    descripcion: ' Pagin Web realiza para que me puedan conocer un poco mas como persona y las cosas que me apasionan realizar.',
+    tecnologias: [' Visual Estudio Code ', ' JavaScript ', ' Netlify '],
+    github: 'https://github.com/hsamayoa-2021462/MipaginaIN6BM.git',
+    demo: 'https://genuine-genie-5ca6fd.netlify.app/',
+    imagenes: [
+      '/assets/images/projects/proyecto-5/captura-1.jpg',
+      '/assets/images/projects/proyecto-5/captura-2.jpg',
+    ],
+  },
+  {
+    id: 'proyecto-6',
+    titulo: ' Agenda Web ',
+    descripcion: ' Una web que te permite tener,agregar, eliminar, actualizar y poner en favoritos a tus contactos, a tu manera y gusto.',
+    tecnologias: [' Visual Estudio Code ', ' JavaScript ', ' Netlify ', ' Git Hub '],
+    github: 'https://github.com/hsamayoa-2021462/AgendaIN6BM.git',
+    demo: 'https://resplendent-sable-aaf6b2.netlify.app/',
+    imagenes: [
+      '/assets/images/projects/proyecto-6/captura-1.jpg',
+      '/assets/images/projects/proyecto-6/captura-2.jpg',
     ],
   },
 ];
